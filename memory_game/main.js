7666e912ad015b343e078f4b559b026777619412
frinci <frinci@gmail.com>
2019-09-30 01:00:26 -0400

    created game logic

---
+++ memory_game/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+let cardOne = "king";
+let cardTwo = "queen";
+let cardThree = "queen";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne)
+console.log("User flipped " + cardFour)
