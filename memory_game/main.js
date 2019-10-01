f45e2b2b4522a4693b72ff661b64a69d8cc1e082
frinci <frinci@gmail.com>
2019-09-30 22:18:21 -0400

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,9 +1,23 @@
-console.log("Up and running!");
+// console.log("User flipped " + cardOne)
+// console.log("User flipped " + cardFour)
 
-let cardOne = "king";
-let cardTwo = "queen";
-let cardThree = "queen";
-let cardFour = "king";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardFour)
+let cardOne = cardsInPlay[0]
+
+ cardsInPlay.push(cardOne);
+
+ console.log("User flipped queen")
+
+ let cardTwo = cardsInPlay[2]
+
+ cardsInPlay.push(cardTwo)
+
+ console.log("User flipped king")
+
+ if (cardsInPlay.length === 2) {
+   if (cardsInPlay[0] === cardsInPlay[1]){
+     alert("You found a match!")
+   } else {alert("Sorry, try again.")}
+ }
