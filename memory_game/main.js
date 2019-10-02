69254099d20f953f0d8f716d33d7152c40e0e5e3
frinci <frinci@gmail.com>
2019-10-02 19:49:54 -0400

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,6 +1,5 @@
 // console.log("User flipped " + cardOne)
 // console.log("User flipped " + cardFour)
-
 let cards = [
   {
     rank: "queen",
@@ -32,6 +31,7 @@ let cardsInPlay = [];
 let cardOne = cardsInPlay[0];
 
 function checkForMatch(){
+  src.setAttribute(cards[cardId].cardImage);
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]){
       alert("You found a match!")
@@ -39,7 +39,8 @@ function checkForMatch(){
   }
 };
 
- function flipCard(cardId){
+ function flipCard(){
+   let cardId = this.getAttribute('data-id', cards[i]);
    console.log("User flipped " + cards[cardId].rank + ".")
    cardsInPlay.push(cards[cardId].rank)
    console.log(cards[cardId].cardImage)
@@ -48,5 +49,14 @@ function checkForMatch(){
    checkForMatch()
  };
 
- flipCard(0);
- flipCard(2);
+ function createBoard(){
+   for (let i = 0; i < cards.length; i++) {
+     let cardElement = document.createElement('img');
+     cardElement.setAttribute("src", "images/back.png");
+     cardElement.setAttribute("data-id", cards[i]);
+     document.addEventListener('click', flipCard)
+     document.cardElement.appendChild("game-board")
+   }
+ }
+
+createBoard()
